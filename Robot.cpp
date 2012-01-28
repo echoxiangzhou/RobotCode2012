09383808c761cfa0c54df79094ed5104015defca
= <rexpert2@gmail.com>
2012-01-27 20:24:48 -0600

    Ported most of the existing robot code.

--- Robot.cpp
+++ Robot.cpp
@@ -1,10 +1,14 @@
+#include "Display.h"
 #include "DriveTrain.h"
+#include "JoystickWrapper.h"
 #include "Logger.h"
 #include "Robot.h"
 #include "Singleton.h"
 
 Robot::Robot()
 {
+	Logger* logger = new Logger("log.txt");
+	Singleton<Logger>::SetInstance(logger);
 	Singleton<Logger>::GetInstance().Logf("Starting the Robot class.");
 }
 
@@ -30,9 +34,23 @@ void Robot::Autonomous()
 
 void Robot::OperatorControl()
 {
+	JoystickWrapper joystick1(0, Attack3);
+	JoystickWrapper joystick2(1, Attack3);
+	
+	Display& display = Singleton<Display>::GetInstance();
+	DriveTrain& driveTrain = Singleton<DriveTrain>::GetInstance();
+	
 	Singleton<Logger>::GetInstance().Logf("Starting operator control.");
 	while( IsOperatorControl() )
 	{
+		float x, y;
+		joystick1.GetAxis(&x, &y);
+		
+		display.Clear();
+		display.PrintfLine(0, "Joystick: (%3f, %3f)", x, y);
+		display.Update();
+		
+		driveTrain.DriveArcade(x, y);
 		Wait(0.01);
 	}
 	Singleton<Logger>::GetInstance().Logf("Stopping operator control.");
